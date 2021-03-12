ae393bdc531a6a68661cc6a2c6af68b1860e16ad
AgolaDeveloper <owinoagola00@gmail.com>
2021-03-12 22:25:35 +0300

    adding functional componenet

---
+++ src/nav.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import './nav.js'
+
+// class Nav extends Component{
+//     constructor(){
+//         super()
+//             this.state={
+
+//             }
+//     }
+// }
+
+function Nav(){
+    render(){
+        return(
+            <div className="divs">
+
+            </div>
+        )
+    }
+}
