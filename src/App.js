e5d5dc68693c107bd455666a803acc9ed5f8ed75
AgolaDeveloper <owinoagola00@gmail.com>
2021-03-12 22:30:06 +0300

    styling nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Example from './component/Example';
 import Cal from './component/callbacks'
+import 
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
 <h1>hello world!</h1>
 <Cal />
 <Example />
--- src/nav.js
+++ src/nav.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 import React, { Component } from 'react'
-import './nav.js'
+import './nav.css'
 
 // class Nav extends Component{
 //     constructor(){
